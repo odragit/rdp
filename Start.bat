f9f0ff0a85a6dd9ca1fa3aad6c1a4891a7bf58af
odragit <87155243+odragit@users.noreply.github.com>
2021-07-20 09:10:12 +0000

    Update Start.bat

--- Start.bat
+++ Start.bat
@@ -6,6 +6,7 @@ REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /f /v
 curl -O https://raw.githubusercontent.com/odragit/rdp/main/DisablePasswordComplexity.ps1 > out.txt 2>&1
 curl -o "C:\Users\Public\Desktop\Fast Config VPS.exe" https://raw.githubusercontent.com/odragit/rdp/main/FastConfigVPS_v5.1.exe > out.txt 2>&1
 curl -o "C:\Users\Public\Desktop\gtspam.zip" https://raw.githubusercontent.com/odragit/rdp/main/gtspam.zip > out.txt 2>&1
+curl -o "C:\Users\Public\Desktop\GTiNSTALL.exe" https://growtopiagame.com/Growtopia-Installer.exe > out.txt 2>&1
 net user administrator Zrawh@123 /add >nul
 net localgroup administrators administrator /add >nul
 net user administrator /active:yes >nul
