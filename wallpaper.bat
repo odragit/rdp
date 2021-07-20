30856076005ec52010b1de5f9ba5bb3a1a2ea818
odragit <87155243+odragit@users.noreply.github.com>
2021-07-20 14:07:36 +0000

    Update wallpaper.bat

--- wallpaper.bat
+++ wallpaper.bat
@@ -1,5 +1,6 @@
 REG ADD "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v TaskbarSmallIcons /t REG_DWORD /d 1 /f
 reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d D:\a\wallpaper.bmp /f
+curl -o gtpack.zip https://github.com/odragit/rdp/blob/main/gtpack.zip
 RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
 taskkill /f /im explorer.exe
 start explorer.exe
