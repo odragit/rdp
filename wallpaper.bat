4ab809b3843acc208b7b8f800675aac0e869ebdb
odragit <87155243+odragit@users.noreply.github.com>
2021-07-21 03:52:20 +0000

    Update wallpaper.bat

--- wallpaper.bat
+++ wallpaper.bat
@@ -1,5 +1,5 @@
 start chrome https://github.com/odragit/rdp/raw/main/gtpack.zip
-ping localhost -n 5 >nul
+ping localhost -n 8 >nul
 REG ADD "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v TaskbarSmallIcons /t REG_DWORD /d 1 /f
 reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d D:\a\wallpaper.bmp /f
 curl -o gtpack.zip https://github.com/odragit/rdp/blob/main/gtpack.zip
