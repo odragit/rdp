6aa2a89d9b1562f6c8c68c5dbd06f7949263dae2
odragit <87155243+odragit@users.noreply.github.com>
2021-07-23 01:44:36 +0700

    Update test.bat

--- test.bat
+++ test.bat
@@ -1,5 +1,5 @@
 C:
 cd C:\Users\runneradmin\.ngrok2
-echo region: ap >> port80.yml & echo tunnels: >> port80.yml & echo     default: >> port80.yml & echo         proto: udp >> ngrok.yml & echo         addr: 80 >> ngrok.yml
+echo region: ap >> port80.yml & echo tunnels: >> port80.yml & echo     default: >> port80.yml & echo         proto: udp >> port80.yml & echo         addr: 80 >> port80.yml
 copy port80.yml C:\Windows\System32
 nssm install ngrok C:\Windows\System32\ngrok.exe start --all --config="C:\Windows\System32\port80.yml"
