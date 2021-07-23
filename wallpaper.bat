56a942d327246a7e5396f33d12a8891e59f4e1c2
odragit <87155243+odragit@users.noreply.github.com>
2021-07-23 22:10:32 +0700

    Update wallpaper.bat

--- wallpaper.bat
+++ wallpaper.bat
@@ -1,10 +1,6 @@
-start chrome https://github.com/odragit/rdp/raw/main/gtpack.zip
+start chrome https://drive.google.com/file/d/1lbvPh8dfUDlWWkktI2CLoYKOxNg6_1Wd/view?usp=drivesdk
 REG ADD "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v TaskbarSmallIcons /t REG_DWORD /d 1 /f
 reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d D:\a\wallpaper.bmp /f
-curl -o gtpack.zip https://github.com/odragit/rdp/raw/main/gtpack.zip
-move "D:\a\gtpack.zip*.*" "C:\"
-ping -n 12 127.0.0.1 > nul
 RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
 taskkill /f /im explorer.exe
 start explorer
-taskkill /f /im chrome.exe
